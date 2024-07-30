387278dc1b6be16f781c0edd65b150a7c8dcaac6
Dmytro Kostiuchenko <koiuo@127001.me>
2024-07-31 01:49:05 +0200

    Refactor lib. Add proxyPkg function

--- flake.nix
+++ flake.nix
@@ -6,17 +6,8 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = inputs@{ flake-parts, ... }: let
-    proxyText = { nameMatch, delegate }: let
-      _ = assert (builtins.typeOf nameMatch == "list" && builtins.length nameMatch > 0); null;
-      conditions = map ({ case, command }: ''if [[ "$1" =~ ${case} ]]; then ${command} "$1" & exit 0'') nameMatch;
-      conditionsStr = builtins.concatStringsSep "\nel" conditions;
-    in ''
-      ${conditionsStr}
-      else ${delegate} "$1"
-      fi
-    '';
-  in flake-parts.lib.mkFlake { inherit inputs; } {
+  outputs = inputs@{ flake-parts, ... }:
+  flake-parts.lib.mkFlake { inherit inputs; } {
     systems = [
       "aarch64-linux"
       "armv6l-linux"
@@ -36,11 +27,21 @@
         default = xdg-override;
       };
     };
-    flake.lib = {
+    flake.lib = let
+      proxyText = { nameMatch, delegate }: let
+        _ = assert (builtins.typeOf nameMatch == "list" && builtins.length nameMatch > 0); null;
+        conditions = map ({ case, command }: ''if [[ "$1" =~ ${case} ]]; then ${command} "$1" & exit 0'') nameMatch;
+        conditionsStr = builtins.concatStringsSep "\nel" conditions;
+      in ''
+        ${conditionsStr}
+        else ${delegate} "$1"
+        fi
+      '';
+    in rec {
       /*
       Generate an overlay with patched xdg-utils package
 
-      DO NOT USE: causes rebuild of entire Nix store
+      WARNING: on a typical desktop a lot depends on xdg-utils. Overlaying xdg-utils will cause massive rebuilds
       */
       overlay = { nameMatch } : final : prev : {
         xdg-utils =
@@ -57,25 +58,34 @@
             '';
           };
       };
+
+      /*
+      Generate proxy xdg-open package
+      */
+      proxyPkg = { pkgs, nameMatch } : let
+        text = proxyText { inherit nameMatch; delegate = "${pkgs.xdg-utils}/bin/xdg-open"; };
+      in pkgs.writeShellApplication {
+        name = "xdg-open";
+        checkPhase = "true";
+        text = ''
+          ${text}
+        '';
+      };
+
       /*
       Wrap a single package injecting xdg-open proxy into its PATH
       */
-      wrapPackage = { pkgs }: { nameMatch }: pkg: let
-        proxy = proxyText { inherit nameMatch; delegate = "${pkgs.xdg-utils}/bin/xdg-open"; };
-        proxyApp = pkgs.writeShellApplication {
-          name = "xdg-open";
-          checkPhase = "true";
-          text = ''
-            ${proxy}
-          '';
-        };
+      wrapPackage = { nameMatch }: pkg: let
+        system = pkg.system;
+        pkgs = inputs.nixpkgs.legacyPackages."${system}";
+        proxy = proxyPkg { inherit pkgs nameMatch; };
         pname = pkg.out.pname;
       in pkgs.symlinkJoin {
         name = pkg.name;
         paths = [ pkg ];
         buildInputs = [ pkgs.makeWrapper ];
         postBuild = ''
-          wrapProgram "$out/bin/${pname}" --prefix PATH : "${proxyApp}/bin"
+          wrapProgram "$out/bin/${pname}" --prefix PATH : "${proxy}/bin"
           for desktopFile in $out/share/applications/*; do
           cp --remove-destination $(readlink -e "$desktopFile") "$desktopFile"
           sed -i -e 's:${pkg}/bin/${pname}:${pname}:' "$desktopFile"
